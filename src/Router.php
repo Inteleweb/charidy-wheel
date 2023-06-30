255c49021bad3f3658b0b63704572c6cce885eb2
Yechiel Weisfish <yechiel@inteleweb.com>
2023-07-01 02:35:37 +0300

    fix

--- src/Router.php
+++ src/Router.php
@@ -1,30 +1,35 @@
 <?php
 
-namespace App;
-
-use App\FundraiserController;
-
 class Router {
-    public function route() {
-        $requestUri = $_SERVER['REQUEST_URI'];
-        $requestMethod = $_SERVER['REQUEST_METHOD'];
-        
-        $segments = explode('/', trim($requestUri, '/'));
-        $controllerName = ucfirst($segments[0] ?? 'Fundraiser') . 'Controller';
-        $action = $segments[1] ?? 'show';
-        
-        $controllerClass = "App\\{$controllerName}";
-        if (!class_exists($controllerClass)) {
-            http_response_code(404);
-            die('404 - Controller not found');
+    private $routes = [];
+
+    public function __construct() {
+        $this->routes = [
+            'fundraiser' => ['controller' => 'FundraiserController', 'method' => 'handle']
+        ];
+    }
+
+    public function dispatch($url) {
+        $url = trim($url, '/');
+        if (array_key_exists($url, $this->routes)) {
+            $controller = $this->routes[$url]['controller'];
+            $method = $this->routes[$url]['method'];
+
+            // Assuming controllers are in the "src" directory and have the namespace "src"
+            $controller = "\\src\\$controller";
+            if (class_exists($controller)) {
+                $object = new $controller;
+                if (method_exists($object, $method)) {
+                    $object->$method();
+                } else {
+                    echo "404 Not Found - Method not found";
+                }
+            } else {
+                echo "404 Not Found - Controller not found";
+            }
+        } else {
+            // Handle not found
+            echo "404 Not Found - Route not found";
         }
-        
-        $controller = new $controllerClass();
-        if (!method_exists($controller, $action)) {
-            http_response_code(404);
-            die('404 - Action not found');
-        }
-        
-        $controller->$action();
     }
 }
