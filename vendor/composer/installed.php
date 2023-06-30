e215a05496a7a1b5c28db0880d66e51b714dd629
Yechiel Weisfish <yechiel@inteleweb.com>
2023-07-01 02:27:43 +0300

    composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'your-username/php-crowdfunding-website',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9dcdce79537bcaf00d1ddd685214d71b0d9e09b7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'your-username/php-crowdfunding-website' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9dcdce79537bcaf00d1ddd685214d71b0d9e09b7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
